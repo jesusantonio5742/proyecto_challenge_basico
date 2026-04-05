924a83fde243776327bb4dc2517d5807fc7adf8b
JesusR <jesus.antonio5742@alumnos.udg.mx>
2026-04-04 22:19:47 -0600

    Resultados/Actualización del challenge

--- upload_results.py
+++ upload_results.py
@@ -1,6 +1,6 @@
 import os
 
-os.system ("git add .")
-os.system ("git commit -m 'Resultados/Actualización del challenge' ")
-os.system ("git branch -M main")
-os.system ("git push origin main")
+os.system ('git add .')
+os.system ('git commit -m "Resultados/Actualización del challenge" ')
+os.system ('git branch -M main')
+os.system ('git push origin main')
